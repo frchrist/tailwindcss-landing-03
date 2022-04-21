459373781e199275aded94aa95b1c2d1fd56a018
christi@an <coding12online@gmail.com>
2022-04-21 11:32:04 +0000

    setup for large screens

---
+++ ReadMe.md
@@ -0,0 +1,15 @@
+
+# Basic Tailwindcss landing page
+
+## setup
+
+### requierement
+- nodejs
+- npm
+
+###	npm  intall
+
+##  npm run watch
+
+
+and open index.html 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode:"jit",
+  content: ["**/*.{html,js}"],
+  darkMode : false,
+ 
+  theme: {
+    extend: {
+      colors : {
+        purple : "#EFCDFF",
+        blue : "#D1F2FF",
+        yellow : "#FBFFE5",
+        gray : "#888888",
+      }
+    },
+  },
+  plugins: [],
+}
